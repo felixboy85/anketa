773d77ac1feb24d3523a440f620b14c27b679a20
Felix <felixboy85@gmail.com>
2021-06-25 22:16:18 +0500

    Fix Input

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let burger = document.querySelector('.hamburger')
 let menu = document.querySelector('.header__list')
 let menuItem = document.querySelectorAll('.header__link')
 
+
 burger.addEventListener('click', () => {    
     menu.classList.toggle('active')
     burger.classList.toggle('open')
@@ -13,3 +14,4 @@ menuItem.forEach((elem)  => {
         burger.classList.toggle('open')
     })
 })
+
